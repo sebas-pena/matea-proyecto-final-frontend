2a81245d6cf2eb4902511c69c828ea7c5c6e7d88
Pablo Peña <insomnionebulous@gmail.com>
2022-09-24 22:06:53 -0300

    Feat: add title

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import ArrowButton from "../buttons/arrow-button/ArrowButton"
 import "./List.css"
-const List = ({ children }) => {
+const List = ({ children, title }) => {
 	return (
-		<div className="list">
-			<ArrowButton direction="left" size="small" />
-			<div className="list__ctn">{children}</div>
-			<ArrowButton size="small" />
+		<div>
+			{title && <h2 className="list__title">{title}</h2>}
+			<div className="list">
+				<ArrowButton direction="left" size="small" />
+				<div className="list__ctn">{children}</div>
+				<ArrowButton size="small" />
+			</div>
 		</div>
 	)
 }
