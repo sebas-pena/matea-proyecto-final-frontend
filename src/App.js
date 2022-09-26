f70bad9bc177e432848aeff62c709cb2c9fb9cbf
Pablo Peña <gmlpubli00@gmail.com>
2022-09-26 18:41:32 -0300

    Feat: use router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
-import Footer from './components/footer/Footer';
-import NavBar from './components/navbar/NavBar';
 import Router from './router/Router';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className='app__page-ctn'>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </div>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
