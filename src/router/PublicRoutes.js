eb8f39bc84ff5591990ba8130de15ff6a4fbe191
juniortejeira <101424705+juniortejeira@users.noreply.github.com>
2022-12-08 13:46:54 -0600

    Add files via upload

--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,18 +1,22 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import HomePage from "../pages/HomePage"
-import LoginPage from "../pages/LoginPage"
+import SingIn from "../pages/SingInPage"
 import ProductPage from "../pages/ProductPage"
+import LoginPage from "../pages/LoginPage"
+import CreateProductsPage from "../pages/CreateProductsPage"
 
 const PublicRoutes = () => {
-  return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="product/:id" element={<ProductPage />} />
-      <Route path="category/:id" element={<ProductPage />} />
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route index element={<HomePage />} />
+			<Route path="login" element={<LoginPage />} />
+			<Route path="sing-in" element={<SingIn />} />
+			<Route path="product" element={<ProductPage />} />
+			<Route path="crear" element={<CreateProductsPage />} />
+			
+		</Routes>
+	)
 }
 
 export default PublicRoutes
